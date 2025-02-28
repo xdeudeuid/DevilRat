8397bbb667958a42974d91c7ed75d1b45921cbde
xdeudeuid <dybadigar935195@gmail.com>
2025-02-28 09:57:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'BOT_TOKEN'
-const id = 'CHAT_ID'
+const token = '7550003731:AAEYdDjfcHN09REbLtROUxKmaOS-lqt5Zjk'
+const id = '6332361146'
 const address = 'https://www.google.com'
 
 const app = express();
